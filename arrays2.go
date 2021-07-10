cbe82452782d4b840cf6a0e7cfc208d6632aafa7
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 02:06:47 -0400

    Aula 26

---
+++ arrays2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Arrays
+  // 1, 1, 2, 3, 5, 8, 13
+  // var numeros = [7]int{1, 1, 2, 3, 5, 8}
+  numeros := []int{1, 1, 2, 3, 5, 8}
+
+  fmt.Println(numeros)
+}
