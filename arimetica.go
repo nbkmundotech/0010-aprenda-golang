7477a1d64283f534c86ea56feebd0905e34eb587
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 01:51:24 -0400

    Aula 15

---
+++ arimetica.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+  // + - * /
+  // %
+  var a int = 23
+  var b int = 7
+
+  // fmt.Println("a + b =", a + b)
+  // fmt.Println("a - b =", a - b)
+  // fmt.Println("a * b =", a * b)
+  fmt.Println("a / b =", a / b)
+
+  fmt.Println("a % b =", a % b)
+}
