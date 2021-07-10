ba3e9d9c0ff840bd25320b131d6817ba7b8e78a6
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 02:22:14 -0400

    Aula 57

---
+++ metodos2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "fmt"
+)
+
+type Pessoa struct {
+  Nome string
+  Sobrenome string
+}
+
+func (p Pessoa) NomeCompleto() string {
+  return p.Nome + " " + p.Sobrenome
+}
+
+func FuncaoNomeCompleto(p Pessoa) string {
+  return p.Nome + " " + p.Sobrenome;
+}
+
+func main() {
+  alguem := Pessoa{"José", "de Alencar"}
+
+  fmt.Println(alguem.NomeCompleto())
+
+  fmt.Println(FuncaoNomeCompleto(alguem))
+}
