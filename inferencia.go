8a616e3eae159173345561647c93212f448f002c
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 01:49:09 -0400

    Aula 10

---
+++ inferencia.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+  // var i int
+  // var j = i // int
+  //
+  // fmt.Printf("Tipo: %T Valor: %v\n", j, j)
+
+  var i = 0.64 + 0.3i
+  fmt.Printf("Tipo: %T Valor: %v\n", i, i)
+}
