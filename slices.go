5d64492c785ec0a90e350344ed5475e8ece7958b
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 02:08:17 -0400

    Aulas 29-30

---
+++ slices.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+                    // 0  1  2  3  4  5  6
+  var numeros = [7]int{1, 1, 2, 3, 5, 8, 13}
+
+  // Slice
+  // fmt.Println(numeros[2:5])
+
+  // fmt.Println(numeros[2:])
+  // fmt.Println(numeros[:5])
+  fmt.Println(numeros[:])
+}
