af256317a3e96359fdc13315ed2567fc6da60f44
NBK Mundo Tech <nbkmundotech@outlook.com>
2021-07-10 01:47:46 -0400

    Aula 07

---
+++ tipos.go
@@ -0,0 +1,27 @@
+
+package main
+
+import "fmt"
+
+func main() {
+  var altura float64 = 1.34
+  var aberto bool = true
+
+  fmt.Printf("Tipo: %T Valor: %v\n", altura, altura)
+  fmt.Printf("Tipo: %T Valor: %v\n", aberto, aberto)
+}
+
+
+// int int8 int16 int32 int64
+//
+// uint uint8 uint16 uint32 uint64
+//
+// string
+//
+// float64       float32
+//
+// bool      true / false
+//
+// complex64 complex128             a + bi
+//
+// byte, rune
